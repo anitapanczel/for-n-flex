b06dedfaace5c0d16ceb93d2f78df98f2f9e4508
anitapanczel <anita@panczel.hu>
2021-11-17 11:23:31 +0100

    flex test

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
-window.addEventListener("load", init);
+const generateArray = function(amount) {
+    let returnArray = [];
 
-function init() {
-    console.log("hello");
+    /*
+        let amountConverted = parseInt(amount);
+        amount = amountConverted;
+    */
+
+    amount = parseInt(amount);
+    // console.log(amount);
+
+    if (!Number.isNaN(amount)) {
+        // console.log("Az amount egy szám.");
+        for (let i = 0; i < amount; i++) {
+            let diff = amount.toString().length - i.toString().length;
+            let addChar = "";
+            for (let j = 0; j < diff; j++) {
+                addChar = addChar + "0";
+            }
+            returnArray.push(addChar + `${i}`);
+
+
+        }
+    } else {
+        // console.log("Az amount nem egy szám.");
+        returnArray.push("error");
+    }
+
+    return returnArray;
 }
+
+const loadEvent = function() {
+    console.log("Az oldal betöltődött.");
+
+    // console.log(generateArray("100"));
+    // console.log(generateArray(15));
+    // console.log(generateArray("kiskutya"));
+    // console.log(generateArray([2]));
+
+    const root = document.getElementById("root");
+    const list = generateArray(100);
+
+    //házi: ugyanannyi karakterből álljanak a tömb elemei, mint ahányat kérünk, hogy generáljunk
+
+    if (list[0] !== "error") {
+        for (const item of list) {
+            root.insertAdjacentHTML("beforeend", `<div>${item}</div>`);
+        }
+    }
+}
+
+window.addEventListener("load", loadEvent);
+
+/*
+window.addEventListener("load", function() {
+    ez is teljesen elfogadott
+})
+
+window.addEventListener("load", () => {
+    arrow function, ez is teljesen elfogadott
+})
+*/
